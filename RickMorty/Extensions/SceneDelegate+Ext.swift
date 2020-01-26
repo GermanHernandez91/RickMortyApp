b28a9f38304536eb5bc9f7c77434539423f5d5d4
German Hernandez <german220291@gmail.com>
2020-01-26 00:54:09 +0000

    Added basic structure and tab bar navigation

---
+++ RickMorty/Extensions/SceneDelegate+Ext.swift
@@ -0,0 +1,68 @@
+//
+//  SceneDelegate+Ext.swift
+//  RickMorty
+//
+//  Created by German Hernandez on 26/01/2020.
+//  Copyright © 2020 German Hernandez. All rights reserved.
+//
+
+import UIKit
+
+extension SceneDelegate {
+    
+    func createTabBar() -> UITabBarController {
+        let tabBar = UITabBarController()
+        configurationTabBar()
+        tabBar.viewControllers = [createCharactersNC(), createLocationsNC(), createEpisodesNC(), createSettingsNC()]
+        
+        return tabBar
+    }
+    
+    
+    func configurationBar() {
+        UINavigationBar.appearance().tintColor          = .systemIndigo
+        UINavigationBar.appearance().backgroundColor    = .systemBackground
+    }
+    
+    
+    func configurationTabBar() {
+        UITabBar.appearance().tintColor = .systemIndigo
+    }
+    
+    
+    func createCharactersNC() -> UINavigationController {
+        let charactersVC        = CharactersVC()
+        charactersVC.title      = "Characters"
+        charactersVC.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person.3.fill"), tag: 0)
+        
+        return UINavigationController(rootViewController: charactersVC)
+    }
+    
+    
+    func createLocationsNC() -> UINavigationController {
+        let locationsVC         = LocationsVC()
+        locationsVC.title       = "Locations"
+        locationsVC.tabBarItem  = UITabBarItem(title: "Locations", image: UIImage(systemName: "map.fill"), tag: 1)
+        
+        return UINavigationController(rootViewController: locationsVC)
+    }
+    
+    
+    func createEpisodesNC() -> UINavigationController {
+        let episodesVC = EpisodesVC()
+        episodesVC.title = "Episodes"
+        episodesVC.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "list.bullet"), tag: 2)
+        
+        return UINavigationController(rootViewController: episodesVC)
+    }
+    
+    
+    func createSettingsNC() -> UINavigationController {
+        let settingsVC = SettingsVC()
+        settingsVC.title = "Settings"
+        settingsVC.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 3)
+        
+        return UINavigationController(rootViewController: settingsVC)
+    }
+    
+}
---
+++ RickMorty/Screens/SettingsVC.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsVC.swift
+//  RickMorty
+//
+//  Created by German Hernandez on 26/01/2020.
+//  Copyright © 2020 German Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureViewController()
+    }
+    
+
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
