d7f3826767e29365fdc472d416ac3ef12de64f9b
German Hernandez <german220291@gmail.com>
2020-01-26 00:22:53 +0000

    Initial Commit

---
+++ RickMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickMorty
+//
+//  Created by German Hernandez on 26/01/2020.
+//  Copyright © 2020 German Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
