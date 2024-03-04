70a9f0ff22f011350729281e513565b50185fc6b
German Hernandez <german220291@gmail.com>
2024-03-04 12:21:58 +0000

    Initial Commit

---
+++ RickMortyCollection/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RickMortyCollection
+//
+//  Created by Germán Hernández del Rosario on 4/3/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
